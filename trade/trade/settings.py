b6d5c8d26baba0c5b1d5444627b1705c30b23082
readke <724222786@qq.com>
2018-12-12 09:23:29 +0800

    日志配置

--- trade/trade/settings.py
+++ trade/trade/settings.py
@@ -123,3 +123,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG' if DEBUG else 'INFO',
+        },
+    },
+}
